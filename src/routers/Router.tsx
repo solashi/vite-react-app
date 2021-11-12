01fd6ccfd2cfb49ec05c85b033223d0875161c8b
Hang <minhhangtranxtnd@gmail.com>
2021-11-12 17:49:22 +0700

    build: create + update instructor

--- src/routers/Router.tsx
+++ src/routers/Router.tsx
@@ -7,6 +7,7 @@ import { Login } from 'screens/auth'
 import { CustomerCompany } from 'screens/customer-company'
 import { Dashboard } from 'screens/dashboard'
 import { Instructor } from 'screens/instructor'
+import { FormInstructor } from 'screens/instructor/FormInstructor'
 import { NoMatch } from 'screens/NoMatch'
 import { RequireAuth } from './RequireAuth'
 
@@ -35,6 +36,8 @@ const Router: React.VFC = () => {
 
         <Route path="/customer-company" element={<CustomerCompany />} />
         <Route path="/instructor" element={<Instructor />} />
+        <Route path="/instructor/create" element={<FormInstructor />} />
+        <Route path="/instructor/edit/:id" element={<FormInstructor />} />
       </Route>
       <Route path="*" element={<NoMatch />} />
     </Routes>
---
+++ src/screens/instructor/FormInstructor.tsx
@@ -0,0 +1,122 @@
+import { yupResolver } from '@hookform/resolvers/yup'
+import { Avatar, Box, Button, Grid, Stack } from '@mui/material'
+import { Input } from 'components/Form'
+import { Page } from 'components/Layouts'
+import {
+  CreateInstructor,
+  createInstructorApi,
+  UpdateInstructor,
+  updateInstructorApi
+} from 'lib/api/instructor'
+import { Instrutor } from 'lib/types'
+import { handleValidateErrors } from 'lib/utils'
+import { SubmitHandler, useForm } from 'react-hook-form'
+import { useMutation, useQuery } from 'react-query'
+import { useNavigate, useParams } from 'react-router'
+import * as yup from 'yup'
+
+const validateInstructor = yup
+  .object({
+    name: yup.string().required('名前は必須入力です。'),
+    name_en: yup.string().required('名前(English)は必須入力です。'),
+    unit_price: yup.string().required(),
+    profile: yup.string().required()
+  })
+  .required()
+
+const FormInstructor: React.VFC = () => {
+  const slug = useParams()
+  const navigate = useNavigate()
+
+  const { control, handleSubmit, setValue, setError } = useForm<
+    CreateInstructor | UpdateInstructor
+  >({
+    defaultValues: {
+      id: Number(slug.id),
+      name: '',
+      name_en: '',
+      unit_price: '',
+      profile: '',
+      image_path: ''
+    },
+    resolver: yupResolver(validateInstructor)
+  })
+
+  useQuery<Instrutor>([`instructors/${slug.id}`], {
+    onSuccess: (data) => {
+      setValue('name', data.name)
+      setValue('name_en', data.name_en)
+      setValue('unit_price', data.unit_price)
+      setValue('profile', data.profile)
+      setValue('image_path', data.image_path)
+    },
+    enabled: !!slug?.id
+  })
+
+  const createInstructor = useMutation(createInstructorApi)
+  const updateInstructor = useMutation(updateInstructorApi)
+  const onSubmit: SubmitHandler<CreateInstructor | UpdateInstructor> = async (values) => {
+    try {
+      if (!slug?.id) {
+        createInstructor.mutateAsync(values)
+      } else {
+        updateInstructor.mutateAsync(values as UpdateInstructor)
+      }
+
+      navigate('/admin-user')
+    } catch (error) {
+      if (error.errors) {
+        handleValidateErrors(error, setError)
+      }
+    }
+  }
+
+  return (
+    <Page title="イベント講師新規登録">
+      <Box component="form" onSubmit={handleSubmit(onSubmit)} noValidate sx={{ mt: 1 }}>
+        <Stack spacing={2} mb={3}>
+          {slug?.id ? (
+            <Avatar sx={{ width: 100, height: 100 }} />
+          ) : (
+            <Input type="file" name="image_path" fullWidth label="名前" control={control} />
+          )}
+
+          <Input fullWidth label="名前" name="name" control={control} />
+
+          <Input fullWidth label="名前(English)" name="name_en" control={control} />
+
+          <Input
+            fullWidth
+            label="プロフィール"
+            name="profile"
+            rows={6}
+            multiline
+            control={control}
+          />
+
+          <Input
+            fullWidth
+            label="1回あたりの報酬金額"
+            name="unit_price"
+            type="number"
+            control={control}
+          />
+        </Stack>
+
+        <Grid container justifyContent="center">
+          {slug?.id ? (
+            <Button type="submit" variant="contained">
+              編集
+            </Button>
+          ) : (
+            <Button type="submit" variant="contained">
+              新規登録
+            </Button>
+          )}
+        </Grid>
+      </Box>
+    </Page>
+  )
+}
+
+export { FormInstructor }
