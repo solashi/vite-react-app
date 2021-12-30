04f39e84ff2f1f526304eefda779eae24170b29f
hoangtrung <trungnh@solashi.com>
2021-12-30 10:11:22 +0700

    chore: filter bar

--- src/components/ReactTable/components/FilterBar.tsx
+++ src/components/ReactTable/components/FilterBar.tsx
@@ -1,14 +1,17 @@
 import { Box, BoxProps, Button, Stack } from '@mui/material'
 import { Input, InputProps, Select, SelectProps } from 'components/Form'
 import { UnknownObj } from 'lib/types'
+import debounce from 'lodash/debounce'
+import { memo, useCallback, useEffect, useMemo } from 'react'
 import { SubmitHandler, useForm } from 'react-hook-form'
 import { Column } from 'react-table'
 
 export type FilterBarColumn = {
   regex?: '_like' | '_equal' | '_between' | '_notEqual' | '_isnull' | 'has_'
   query_key?: string
-  type?: 'select' | 'text' | 'radio'
+  searchType?: 'select' | 'text' | 'radio'
   additionSearchProps?: Partial<SelectProps<UnknownObj, UnknownObj> | InputProps<UnknownObj>>
+  search?: boolean
 }
 
 export type FilterBarProps<T extends UnknownObj> = BoxProps<
@@ -16,38 +19,67 @@ export type FilterBarProps<T extends UnknownObj> = BoxProps<
   {
     searchColumns: Column<T>[]
     handleChangeParams: (params: UnknownObj) => void
+    watchMode?: boolean
   }
 >
 
-function FilterBar<T extends UnknownObj>({
+function FilterBarComponent<T extends UnknownObj>({
   handleChangeParams,
   searchColumns,
+  watchMode,
   ...props
 }: FilterBarProps<T>) {
-  const { control, handleSubmit } = useForm<UnknownObj>({
+  const { control, handleSubmit, watch } = useForm<UnknownObj>({
     defaultValues: searchColumns.reduce((df, cur) => {
       ;(df as UnknownObj)[cur['accessor'] as string] = ''
       return df
     }, {})
   })
 
-  const getSearchObj = (key: string) => {
-    return searchColumns.find((el) => el.accessor === key)
-  }
+  const getSearchObj = useCallback(
+    (key: string) => {
+      return searchColumns.find((el) => el.accessor === key)
+    },
+    [searchColumns]
+  )
 
-  const getQueryParams = (values: UnknownObj) => {
-    const params = Object.keys(values).reduce<UnknownObj>((_params, cur) => {
-      if (values[cur]) {
-        const searchObj = getSearchObj(cur)
-        const regex = searchObj?.regex || '_like'
-        const query_key = searchObj?.query_key || cur
-        _params[`${query_key}${regex}`] = values[cur]
+  const getQueryParams = useCallback(
+    (values: UnknownObj) => {
+      const params = Object.keys(values).reduce<UnknownObj>((_params, cur) => {
+        if (values[cur]) {
+          const searchObj = getSearchObj(cur)
+          const regex = searchObj?.regex || '_like'
+          const query_key = searchObj?.query_key || cur
+          _params[`${query_key}${regex}`] = values[cur]
+        }
+        return _params
+      }, {})
+
+      return params
+    },
+    [getSearchObj]
+  )
+
+  const debounceChange = useMemo(
+    () => debounce((params) => handleChangeParams(params), 300),
+    [handleChangeParams]
+  )
+
+  useEffect(() => {
+    if (!watchMode) return
+    const subscription = watch((value, { name }) => {
+      const searchObj = getSearchObj(name as string)
+      const hasDebounce = searchObj?.searchType === 'text' || searchObj?.searchType === undefined
+      const params = getQueryParams(value)
+      if (hasDebounce) {
+        debounceChange(params)
+      } else {
+        handleChangeParams(params)
       }
-      return _params
-    }, {})
+    })
 
-    return params
-  }
+    return () => subscription.unsubscribe()
+  }, [debounceChange, getQueryParams, getSearchObj, handleChangeParams, watch, watchMode])
 
   const onSubmit: SubmitHandler<UnknownObj> = (values) => {
     const params = getQueryParams(values)
@@ -63,28 +95,36 @@ function FilterBar<T extends UnknownObj>({
       {...props}
     >
       <Stack direction="row" alignItems="center" spacing={2}>
-        {searchColumns.map(({ accessor, Header, type, additionSearchProps }, index) => {
-          const controlProps = {
-            name: accessor as string,
-            label: Header as string,
-            control,
-            key: index
-          }
+        {searchColumns.map(
+          ({ accessor, Header, searchType, additionSearchProps, search = true }, index) => {
+            const controlProps = {
+              name: accessor as string,
+              label: Header as string,
+              control,
+              key: index
+            }
 
-          switch (type) {
-            case 'select':
-              return <Select {...controlProps} {...additionSearchProps} />
-            default:
-              return <Input {...controlProps} {...additionSearchProps} />
-          }
-        })}
+            if (!search) return null
 
-        <Button type="submit" variant="contained">
-          検索
-        </Button>
+            switch (searchType) {
+              case 'select':
+                return <Select size="small" {...controlProps} {...additionSearchProps} />
+              default:
+                return <Input size="small" {...controlProps} {...additionSearchProps} />
+            }
+          }
+        )}
+
+        {!watchMode && (
+          <Button type="submit" variant="contained">
+            検索
+          </Button>
+        )}
       </Stack>
     </Box>
   )
 }
+
+const FilterBar = memo(FilterBarComponent) as typeof FilterBarComponent
 
 export { FilterBar }
